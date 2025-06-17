d4195991c29193edff3cb133d884dc24b84b8d3e
Chao-ying Fu <cfu@wavecomp.com>
2025-06-17 15:00:02 -0700

    Add a dts file for Boston boards and build it.

--- platform/generic/mips/objects.mk
+++ platform/generic/mips/objects.mk
@@ -5,4 +5,5 @@
 ifeq ($(PLATFORM_RISCV_XLEN), 64)
 carray-platform_override_modules-$(CONFIG_PLATFORM_MIPS_P8700) += mips_p8700
 platform-objs-$(CONFIG_PLATFORM_MIPS_P8700) += mips/p8700.o
+platform-dtb-$(CONFIG_PLATFORM_MIPS_P8700) += mips/mips,boston-p8700.dtb
 endif
